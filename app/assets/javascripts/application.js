dc99c7cc45942c357eeaff043a110d2c43460a8d
Max Ono <mono@vistahigherlearning.com>
2015-10-30 11:27:47 -0700

    uses cool login/registion js widget

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -47,3 +47,22 @@ $(document).ready(function() {
     
     
 });
+
+$(function() {
+
+    $('#login-form-link').click(function(e) {
+		$("#login-form").delay(100).fadeIn(100);
+ 		$("#signup-form").fadeOut(100);
+		$('#signup-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+	$('#signup-form-link').click(function(e) {
+		$("#signup-form").delay(100).fadeIn(100);
+ 		$("#login-form").fadeOut(100);
+		$('#login-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+
+});
