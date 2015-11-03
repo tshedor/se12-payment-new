836c2a76d358c050fa3ed777779962ecc0d98dd6
Max Ono <mono@vistahigherlearning.com>
2015-11-02 20:41:05 -0800

    displays reg errors as flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,16 +26,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
       if @user.save
         session[:user_id] = @user.id
-        format.html { redirect_to payments_path }
-        format.json { render :show, status: :created, location: @user }
+        redirect_to payments_path
       else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        flash.now[:error] = @user.errors.full_messages.first
+        render :login
       end
-    end
   end
 
   # PATCH/PUT /users/1
