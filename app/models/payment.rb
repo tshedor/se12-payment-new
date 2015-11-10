1ffd24b6c16651e531777e6c46a5be4bbd1fd826
Max Ono <mono@vistahigherlearning.com>
2015-11-10 10:35:01 -0800

    uses names instead of ids to fill selects

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,6 +2,6 @@ class Payment < ActiveRecord::Base
 	has_many :payment_users
 	has_many :users, through: :payment_users
 	belongs_to :type
-	validates_presence_of :title, :recipient_id, :amount, :type_id
+	validates_presence_of :title, :recipient_id, :amount
 	
 end
