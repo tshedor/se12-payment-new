8071ca89974b7dfb7e769363a58bd7b26ce7230f
Max Ono <mono@vistahigherlearning.com>
2015-11-13 22:10:11 -0800

    adds a lot of form styles

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,6 +2,9 @@ class Payment < ActiveRecord::Base
 	has_many :payment_users
 	has_many :users, through: :payment_users
 	belongs_to :type
-	validates_presence_of :sender_id, :recipient_id, :amount, :note
+	validates_presence_of :sender_id, :recipient_id, :note
+	validates :amount, :presence=>true,
+						:numericality => true
+  validates_format_of :amount, :with => /\A\d+(?:\.\d{0,2})?\z/
 	
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,8 @@ class User < ActiveRecord::Base
 	  return nil
 	end
 
+	def full_name 
+		"#{first_name} #{last_name}"
+	end
+
 end
